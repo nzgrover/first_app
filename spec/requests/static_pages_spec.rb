33e3e19d4ff900c46b07865ba4731750d2773d45
Trevor Hills <trevorh@ctek.co.nz>
2014-07-28 14:22:05 +1200

    Finished Static Pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,6 +9,10 @@ describe "Static Pages" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
     end
+		it "should have the title 'Home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Home")
+    end					
   end
 	describe "Help page" do
     it "should have the content 'Help'" do
@@ -16,7 +20,11 @@ describe "Static Pages" do
       #get static_pages_index_path
       #response.status.should be(200)
 			visit '/static_pages/help'
-			expect(page).to have_content('Help')
+			expect(page).to have_content('Help')	
+    end	
+		it "should have the title 'Help'" do
+		  	visit '/static_pages/help'
+			expect(page).to have_title("Help")
     end
   end
 	describe "About Page" do
@@ -27,6 +35,11 @@ describe "Static Pages" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
     end
+    it "should have the title 'About Us'" do
+		  	visit '/static_pages/about'
+			expect(page).to have_title("About Us")
+    end
+
   end
 
 end
